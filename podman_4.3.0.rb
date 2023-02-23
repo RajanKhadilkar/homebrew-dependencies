e49993721cf0a241d11f1df015c807b84ab82d47
RajanKhadilkar <60446474+RajanKhadilkar@users.noreply.github.com>
2023-02-23 12:44:22 +0530

    Update podman_4.3.0.rb

--- podman_4.3.0.rb
+++ podman_4.3.0.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Podman430 < Formula
-  desc ""
-  homepage ""
+  desc "Podman version 4.3.0"
+  homepage "https://github.com/RajanKhadilkar/homebrew-podman"
   url "https://github.com/containers/podman/archive/refs/tags/v4.3.0.tar.gz"
   sha256 "55a3a09b80f23f78aaeb74fbf878fa0b1ef1842e5b282ad61e82a9dc4c74bf34"
-  license ""
+  license "MIT"
 
   # depends_on "cmake" => :build
 
